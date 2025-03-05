b58dbaa2d9607d0b4d5029cc143194b0a99bdf05
sebaspatric <sebastian.menares@gmail.com>
2025-03-05 19:05:26 -0300

    audio

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ window.onload = function() {
   // Intentar iniciar la reproducción de la canción
   player.postMessage('{"command":"play"}', '*');
 };
+document.addEventListener('DOMContentLoaded', function() {
+  function playAudio() {
+    var audio = document.getElementById('audio-player');
+    audio.play().catch(function(error) {
+      console.log('Error al reproducir audio:', error);
+    });
+    document.removeEventListener('click', playAudio);
+  }
+
+  document.addEventListener('click', playAudio);
+});
